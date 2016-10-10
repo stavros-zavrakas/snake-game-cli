5c4091112fa861ca2d08f0bdcc87904cf8472160
Stavros Zavrakas <szavrakas@cambridge.org>
2016-10-10 11:43:17 +0100

    start splitting the files

---
+++ axis.js
@@ -0,0 +1,27 @@
+let axis = {
+  x: 1,
+  y: 1
+};
+
+// moveUp and moveDown are acting against axis.y
+function moveUp () {
+  if (axis.y < 1) {
+    axis.y = 1;
+  }
+}
+
+function moveDown () {
+  if (axis.y < 1) {
+    axis.y = 1;
+  }  
+}
+
+// moveRight and moveLeft are acting against axis.x
+function moveRight () {
+  
+}
+
+function moveLeft () {
+  
+}
+
---
+++ config.js
@@ -0,0 +1,8 @@
+// size of frame
+const WIDTH = 120;
+const HEIGHT = 40;
+
+module.exports = {
+  WIDTH: WIDTH,
+  HEIGHT: HEIGHT
+};
