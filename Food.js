d85f21204e26cc7a3f1123f0600f6ab35eec7911
Stavros Zavrakas <szavrakas@cambridge.org>
2016-10-18 13:28:33 +0100

    extra helpers added

--- Food.js
+++ Food.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const helpers = require('./foodHelpers');
 const Point = require('./Point');
 const libs = require('./libs');
 const c = require('./constants');
@@ -13,34 +14,9 @@ class Food {
   }
 
   place(snakeBody) {
-    // @todo: check that we don't place it over the snake!
-    let i, bodyPiece, conflict = false;
-    let x = libs.getRandomInt(1, config.WIDTH - 2);
-    let y = libs.getRandomInt(1, config.HEIGHT - 2);
+    let point = helpers.findFreeSpaceForFood(snakeBody);
 
-    while (1) {
-      // scan to check if there is conflict with the body of the snake
-      for (i = 0; i < snakeBody.length; i++) {
-        bodyPiece = snakeBody[i];
-        conflict = (x === bodyPiece.getX() && y === bodyPiece.getY());
-        // If we find a conflict with a part of the body, there is no
-        // reason to continue the iteration
-        if (conflict) {
-          break;
-        }
-      }
-
-      // If there is conflict, pick random point again, otherwise break
-      if (conflict) {
-        x = libs.getRandomInt(1, config.WIDTH - 2);
-        y = libs.getRandomInt(1, config.HEIGHT - 2);
-        conflict = false;
-      } else {
-        break;
-      }
-    }
-
-    this.current = new Point(x, y);
+    this.current = new Point(point.x, point.y);
 
     this.history.push(this.current);
   }
---
+++ foodHelpers.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const config = require('./config');
+const libs = require('./libs');
+
+// Ensures that we don't place the food over the snake
+function findFreeSpaceForFood (snakeBody) {
+  let i;
+  let bodyPiece;
+  let conflict = false;
+
+  let x = libs.getRandomInt(1, config.WIDTH - 2);
+  let y = libs.getRandomInt(1, config.HEIGHT - 2);
+
+  while (1) {
+    // iterate over the snakeBody and check if 
+    // there is conflict with the random point
+    for (i = 0; i < snakeBody.length; i++) {
+      bodyPiece = snakeBody[i];
+      conflict = (x === bodyPiece.getX() && y === bodyPiece.getY());
+      // break if there is a conflict, there is no
+      // reason to continue the iteration
+      if (conflict) {
+        break;
+      }
+    }
+
+    // pick random point again in case of conflict
+    if (conflict) {
+      x = libs.getRandomInt(1, config.WIDTH - 2);
+      y = libs.getRandomInt(1, config.HEIGHT - 2);
+      conflict = false;
+    } else {
+      break;
+    }
+  }
+
+  return {
+    x: x,
+    y: y
+  };
+}
+
+module.exports = {
+  findFreeSpaceForFood: findFreeSpaceForFood
+};
